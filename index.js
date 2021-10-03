7a6ac4f37217734b17bfb26fe8326c80195e4201
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-10-03 19:23:08 -0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+const pagination = async (msg, pages, emojiList = ['⏪', '⏩'], timeout = 60000, endPage = undefined) => {
+	if (!msg && !msg.channel) {
+		throw new Error('Is not possible send a message in this channel.');
+	} else if (!pages) {
+		throw new Error('.');
+	} else if (emojiList.length !== 2) {
+		throw new Error('Need two emojis.');
+	}
+	
+	let page = 0;
+	const currentPage = await msg.channel.send({ embeds: [pages[page].setFooter(`Page ${page + 1} / ${pages.length}`)] });
+
+	if (pages.length > 1) {
+		for (const emoji of emojiList) {
+			await currentPage.react(emoji);
+		}
+	}
+
+	const reactionCollector = currentPage.createReactionCollector({ time: timeout });
+
+	reactionCollector.on('collect', (reaction, user) => {
+		if(!user.bot) {
+			reaction.users.remove(user);
+		}
+
+		if(user.id != msg.author.id) {
+			return;
+		}
+
+		switch (reaction.emoji.name) {
+			case emojiList[0]:
+				page = page > 0 ? --page : pages.length - 1;
+
+				break;
+			case emojiList[1]:
+				page = page + 1 < pages.length ? ++page : 0;
+
+				break;
+			default:
+				break;
+		}
+
+		currentPage.edit({ embeds: [pages[page].setFooter(`Page ${page + 1} / ${pages.length}`)] });
+	});
+
+	reactionCollector.on('end', () => {
+		if (!currentPage.deleted) {
+			if(endPage) {
+				currentPage.edit({embeds: [endPage]});
+			}
+	
+			currentPage.reactions.removeAll()
+		}
+	});
+
+	return currentPage;
+};
+
+module.exports = pagination;
